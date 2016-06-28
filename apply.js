3eb45b037f287bf08e5af722da914b22591f8757
James Lo <jlcooly@gmail.com>
2016-06-27 18:53:23 -0700

    working on apply form

---
+++ apply.js
@@ -0,0 +1,15 @@
+function submitForm(){
+	var userInformation = {
+	userName : document.getElementById('user_name').value,
+	userAge :document.getElementById('user_age').value,
+	userAddress :document.getElementById('user_address').value,
+	userCity : document.getElementById('user_city').value,
+	userState :document.getElementById('user_state').value
+};
+console.log(submitForm());
+	prompt (userInformation);
+};
+	
+	
+	
+
